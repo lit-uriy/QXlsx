95570c0f38e42b062953611707ea3d7575a3517f
Yuriy (Юрий) <lit-uriy@sibna.ru>
2024-04-26 15:17:58 +0500

    XlsxWrapper: Нахомячиваю классы.

--- XlsxWrapper/xlsx_wrapper.cpp
+++ XlsxWrapper/xlsx_wrapper.cpp
@@ -1,12 +1,108 @@
 #include "xlsx_wrapper.h"
 
+//==================================================================================================
+//                            Book
+//==================================================================================================
+
 XlsxWrapper::Book::Book(QString fileName)
     :_valid(false)
 {
-    _protocolFile = new QXlsx::Document(fileName);
-    if (!_protocolFile->isLoadPackage()) {
+    _bookFile = new QXlsx::Document(fileName);
+    if (!_bookFile->isLoadPackage()) {
         qDebug() << "XlsxWrapper::Book::Book() ERROR on open XLSX-file (" << fileName << ")";
         return;
     }
     _valid = true;
+
+    QList<QString> sheetNames();
+    foreach (QString name, sheetNames()) {
+        Sheet *s = new Sheet(name, this);
+        s->_xlsxSheet = dynamic_cast<QXlsx::Worksheet *>(_bookFile->sheet(name));
+        _sheets.insert(name, s);
+    }
+}
+QList<XlsxWrapper::Sheet*> XlsxWrapper::Book::sheets()
+{
+    return _sheets.values();
+}
+
+QList<QString> XlsxWrapper::Book::sheetNames()
+{
+    return _sheets.keys();
+}
+
+
+XlsxWrapper::Sheet* XlsxWrapper::Book::sheet(QString asheetName)
+{
+    return _sheets.value(asheetName);
+}
+
+
+
+//==================================================================================================
+//                            Sheet
+//==================================================================================================
+
+XlsxWrapper::Sheet::Sheet(QString aname, Book abook)
+    :_name(anme)
+    ,_book(abook)
+{
+    _cellList = _xlsxSheet->getFullCells( &_maxRow, &_maxCol );
+}
+
+XlsxWrapper::Cell* XlsxWrapper::Sheet::cell(int rowIndex, int colIndex)
+{
+    Cell *c = nullptr;
+
+    for ( int index = 0; index < _cellList.size(); ++index ) {
+        QXlsx::CellLocation location = _cellList.at(index); // cell location
+        if ((location.row == rowIndex) && (location.col == colIndex)){
+            // Вернём найденную ячеку
+            с = new Cell();
+            c->_row = location.row;
+            c->_col = location.col;
+            c->_sheet = this;
+            c->_data = location.cell->value();
+            return c;
+        }
+    }
+
+    return c;
+}
+
+XlsxWrapper::Cell* XlsxWrapper::Sheet::findCell(QString text, FindRules fr = Contains, Qt::CaseSensitivity cs = Qt::CaseSensitive)
+{
+    QXlsx::Cell *outCell = nullptr;
+
+    for ( int index = 0; index < _cellList.size(); ++index ) {
+        QXlsx::CellLocation location = _cellList.at(index); // cell location
+
+        // value of cell
+        QVariant var = location.cell->value();
+        QString str = var.toString();
+        auto ptr = location.cell;
+
+        switch (fr) {
+        case StartsWith:
+            if (str.startsWith(text, cs)){
+                outCell = ptr.get();
+                return outCell;
+            }
+            break;
+        case Contains:
+            if (str.contains(text, cs)){
+                outCell = ptr.get();
+                return outCell;
+            }
+            break;
+        default: // EndsWith
+            if (str.endsWith(text, cs)){
+                outCell = ptr.get();
+                return outCell;
+            }
+            break;
+        }
+    }
+
+    return outCell;
 }
--- XlsxWrapper/xlsx_wrapper.h
+++ XlsxWrapper/xlsx_wrapper.h
@@ -17,37 +17,54 @@ public:
     class Book
     {
         QXlsx::Workbook *_xlsxBook;
-        QXlsx::Document *_protocolFile;
-        QVector<QXlsx::CellLocation> _cellList;
+        QXlsx::Document *_bookFile;
         bool _valid;
+        QMap<QString, Sheet*> _sheets; // <name, sheet pointer>
 
     public:
         Book(QString fileName);
-        bool isValid();
+        bool isValid() {return _valid;}
+
+        Sheet* sheet(QString asheetName);
         QList<Sheet*> sheets();
         QList<QString> sheetNames();
 
-        QXlsx::Workbook* toXlsx();
+        QXlsx::Workbook* toXlsx(){return _xlsxBook;}
     };
 
     class Sheet
     {
-        QXlsx::Worksheet *xlsxSheet;
+        friend class Book;
+
+        Book *_book;
+        QString _name;
+        QXlsx::Worksheet *_xlsxSheet;
+        QVector<QXlsx::CellLocation> _cellList;
+        int _maxRow;
+        int _maxCol;
+
+        Sheet(QString aname, Book abook);
 
     public:
-        QXlsx::Worksheet* toXlsx();
+        Book* book(){return _book;}
+        Cell* cell(int rowIndex, int colIndex);
+//        Cell* cell(QString coordinat); // буквенно цифровые координаты, например: B7, столбец "B", строка 7
 
         Cell* findCell(QString text, FindRules fr = Contains, Qt::CaseSensitivity cs = Qt::CaseSensitive);
+
+        QXlsx::Worksheet* toXlsx(){return _xlsxSheet;}
     };
 
     class Cell
     {
+        friend class Sheet;
+
         int _row, _col;
         Sheet *_sheet;
+        QVariant _data;
+        Cell(){}
 
     public:
-        Cell(): _row(-1), _col(-1)
-        {}
 
         QVariant data();
         void setData(QVariant adata);
