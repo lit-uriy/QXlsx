900ffd2d98d662a9f61a3d9e423dca0651863192
Yuriy (Юрий) <lit-uriy@sibna.ru>
2024-04-26 09:21:15 +0500

    Заготовка Обёртки для QXlsx.

---
+++ XlsxWrapper/xlsx_wrapper.cpp
@@ -0,0 +1,6 @@
+#include "xlsx_wrapper.h"
+
+XlsxWrapper::XlsxWrapper()
+{
+
+}
---
+++ XlsxWrapper/xlsx_wrapper.h
@@ -0,0 +1,11 @@
+#ifndef XLSX_WRAPPER_H
+#define XLSX_WRAPPER_H
+
+
+class XlsxWrapper
+{
+public:
+    XlsxWrapper();
+};
+
+#endif // XLSX_WRAPPER_H
